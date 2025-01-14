4360c9782dd7ed8e918af6e9342bc5a9831f91e6
vagrant-soul <77929833@qq.com>
2025-01-14 08:19:12 +0000

    first

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,63 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
+import { ion, resolve } from "starlight-ion-theme";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'POE2备忘录',
+			logo: {
+				src: './src/assets/logo.webp'
+			  },
+			pagination: false,
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/custom.css',
+			],			
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: '[home] Home',
+    				link: '/'
 				},
 				{
-					label: 'Reference',
+					label: '[home] 测试测试测试',
+    				link: '/getting-started/'
+				},
+				{
+					label: '[home] 另外一个测试',
+    				link: '/changelog/',
+					badge: {
+						variant: 'tip',
+						text: 'New'
+					  }
+				},
+				{
+					label: '内容集合',
 					autogenerate: { directory: 'reference' },
 				},
 			],
+			plugins: [
+				ion({
+					icons: {
+						iconPath: resolve("./src/icons", import.meta.url)
+					  },
+					footer: {
+					text: '©️ 2025 Vagrant Soul',
+					links: [{
+						text: '流放备忘录',
+						href: 'https://louisescher.dev',
+					}],
+					icons: [{
+						name: 'github',
+						href: 'https://github.com/louisescher/starlight-ion-theme'
+					}]
+					}
+				  })
+			  ]
 		}),
 	],
 });
